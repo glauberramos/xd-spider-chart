69777890f0f97a430d5ca9c073cdaa75b2907b10
Gabriel Albo <gabriel@albo.com.br>
2013-11-12 22:40:10 -0200

    Mapping blue and red colors, filling the chart

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(function () {
-	$('#d1').css('fill', 'red');
-	$('#d2').css('fill', 'red');
-	$('#d3').css('fill', 'red');
-	$('#d4').css('fill', 'red');
-	$('#d5').css('fill', 'red');
+	var blue = '#3498db';
+	var red = '#e74c3c';
+
+	$('#a2').css('fill', blue);
+	$('#b2').css('fill', blue);
+	$('#c2').css('fill', blue);
+	$('#d4').css('fill', blue);
+	$('#e3').css('fill', blue);
+
+	$('#c3').css('fill', red);
+	$('#d5').css('fill', red);
+	$('#e5').css('fill', red);
 });
