9331dbc1408b10a212e1e9bcdc896152974b867f
Glauber Ramos <glauberamos@gmail.com>
2015-01-05 20:42:01 -0200

    adding parse and reading from database

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
+Parse.initialize("SvIXm6lpaiR9e9hnajEicOGVr9tQAfYDMxi5Wzuz", "jZMWX0q6byyuaPl1TK9hftFwoVonZznBwCjINbuN");
+
+var Spider = Parse.Object.extend("Spider");
+
+var query = new Parse.Query(Spider);
+
+query.find({
+  success: function(results) {
+    $(results).each(function() {
+    	this.get('name')
+    	this.get('front')
+    	this.get('research')
+    	this.get('visual')
+    	this.get('interaction')
+    	this.get('product')
+	});
+  },
+  error: function(error) {
+    console.log("Error: " + error.code + " " + error.message);
+  }
+});
+
 $(function () {
 	var blue = '#3498db';
-	var red = '#e74c3c';
 
-	$('#a2').css('fill', blue);
-	$('#b2').css('fill', blue);
-	$('#c2').css('fill', blue);
-	$('#d4').css('fill', blue);
-	$('#e3').css('fill', blue);
-
-	$('#c3').css('fill', red);
-	$('#d5').css('fill', red);
-	$('#e5').css('fill', red);
+	query.find({
+		success: function(results) {
+			$(results).each(function() {
+				// $('#a' + this.get('name')).css('fill', blue);
+				$('#a' + this.get('product')).css('fill', blue);
+				$('#b' + this.get('research')).css('fill', blue);
+				$('#c' + this.get('visual')).css('fill', blue);
+				$('#d' + this.get('front')).css('fill', blue);
+				$('#e' + this.get('interaction')).css('fill', blue);
+			});
+		},
+		error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		}
+	});
 });
